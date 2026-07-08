a93e5aa669ad4ac6c4a126fad8ba1093ebe7d4d4
Hornburg Bjoern (ETAS-ECM XPC-Hi) <hrb2hi@bosch.com>
2026-07-08 14:23:55 +0200

    Add cmdline parser for ethernet interface

--- score/time_slave/src/application/time_slave.cpp
+++ score/time_slave/src/application/time_slave.cpp
@@ -15,6 +15,7 @@
 #include "score/mw/log/logging.h"
 #include "score/time_slave/src/common/logging_contexts.h"
 
+#include <iostream>
 #include <thread>
 
 namespace score
@@ -28,12 +29,26 @@ namespace
 constexpr std::int32_t kInitSuccess = 0;
 constexpr std::int32_t kInitFailure = -1;
 
+void PrintUsage(const std::string& app_name)
+{
+    std::cout
+        << "Usage: " << app_name
+        << " [options]\n"
+           "Options:\n"
+           "  -h, --help Print this message and exit.\n"
+           "  -i, --interface <iface> Define the ethernet interface to be used (default is \""
+              << details::GptpEngineOptions().iface_name <<"\").\n";
+}
+
 }  // namespace
 
 TimeSlave::TimeSlave() = default;
 
-std::int32_t TimeSlave::Initialize(const score::mw::lifecycle::ApplicationContext& /*context*/)
+std::int32_t TimeSlave::Initialize(const score::mw::lifecycle::ApplicationContext& context)
 {
+    if (!ParseCmdLineArgs(context))
+        return kInitFailure;
+
     engine_ = std::make_unique<details::GptpEngine>(opts_);
 
     if (!engine_->Initialize())
@@ -50,6 +65,39 @@ std::int32_t TimeSlave::Initialize(const score::mw::lifecycle::ApplicationContex
 
     score::mw::log::LogInfo(kTimeSlaveAppContext) << "TimeSlave initialized";
     return kInitSuccess;
+}
+
+bool TimeSlave::ParseCmdLineArgs(const score::mw::lifecycle::ApplicationContext& context)
+{
+    const auto& args = context.get_arguments();
+    auto iter = args.cbegin();
+    const auto& app_name = *iter;
+
+    for (++iter; iter != args.cend(); ++iter)
+    {
+        if (*iter == "-h" || *iter == "--help")
+        {
+            PrintUsage(app_name);
+            return false;
+        }
+        else if (*iter == "-i" || *iter == "--interface")
+        {
+            ++iter;
+            if (iter == args.cend())
+            {
+                PrintUsage(app_name);
+                return false;
+            }
+            opts_.iface_name = *iter;
+        }
+        else
+        {
+            std::cerr << "Unknown argument \"" << *iter << "\"." << std::endl;
+            PrintUsage(app_name);
+            return false;
+        }
+    }
+    return true;
 }
 
 std::int32_t TimeSlave::Run(const score::cpp::stop_token& token)
