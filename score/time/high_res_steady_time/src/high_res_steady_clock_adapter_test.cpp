dbd43e90ca1989c8682937947ced417e9d794aa1
Gabriel Pinheiro <gpn1brg@bosch.com>
2026-09-16 15:52:47 +0100

    satisfy clang-format for high_res_steady_clock_adapter_test

--- score/time/high_res_steady_time/src/high_res_steady_clock_adapter_test.cpp
+++ score/time/high_res_steady_time/src/high_res_steady_clock_adapter_test.cpp
@@ -31,8 +31,9 @@ TEST(HighResSteadyClockTest, NowReturnsTimepointSuitableForDurationArithmetic)
     ::testing::Test::RecordProperty("PartiallyVerifies", "comp_req__high_res_steady_time__snapshot");
     ::testing::Test::RecordProperty("TestType", "requirements-based");
     ::testing::Test::RecordProperty("DerivationTechnique", "requirements-analysis");
-    ::testing::Test::RecordProperty("Description",
-                                    "HighResSteadyClock::Now returns a timepoint suitable for duration arithmetic without requiring Init.");
+    ::testing::Test::RecordProperty(
+        "Description",
+        "HighResSteadyClock::Now returns a timepoint suitable for duration arithmetic without requiring Init.");
 
     auto mock = std::make_shared<HighResSteadyClockBackendMock>();
     test_utils::ScopedClockOverride<HighResSteadyTime> guard{mock};
